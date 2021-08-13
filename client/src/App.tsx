50a5621fa8de5049a0f65ae6fbc51a3ae24a0b5a
KIMBEOBWOO <qjqdn1568@naver.com>
2021-08-13 21:02:07 +0900

    add app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,47 @@
+import { Button, Checkbox, Typography } from '@material-ui/core';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+/**
+ * (void) => JSX.Element
+ * 함수란? 입력값을 받아서, 어떠한 처리를 한 뒤, 적절한 출력값을 내뱉는 로직
+ * @returns JSX.Element
+ */
+function App():JSX.Element {
+  /* state, props, hook */
+
+  /**
+   * @name state란?
+   * 상태 == 데이터 이다.
+   * 데이터는 화면의 레이아웃 혹은 태그 안에 들어가는 값, 변수이다.
+   */
+  const var1 = 50;
+
+  /* [변수, 변수를 바꿀수 있는 함수] = React.useState(초기값); */
+  const [var2, setVar2] = React.useState<number>(30);
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-evenly',
+      backgroundColor: 'tomato',
+      flexDirection: 'row'
+    }}>
+      {/* 번호 자리 */}
+      <Typography variant="h1">
+        {/* var2 출력 하는 곳 */}
+        {var2}
+      </Typography>
+
+      <Button 
+        variant="contained"
+        onClick={() => {
+          const newTemp = var2 + 1;
+          setVar2(newTemp);
+        }}
+      >add</Button>
     </div>
   );
 }
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,8 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+/**
+ * @name index.tsx
+ * HTML 문서 - 전체 개발한 웹 페이지가 HTML 도큐먼트(문서) 형태로 저장된다.
+ * 해당 HTML 문서를 웹 (인터넷) 상에서 보여주는 역할을 한다.
+ */
 ReactDOM.render(
   <React.StrictMode>
+    {/**
+     * 입력값 : void
+     * 출력값 : 웹 상에서 보여지는 요소를 반환.
+     */}
     <App />
   </React.StrictMode>,
   document.getElementById('root')
